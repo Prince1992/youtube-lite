ba2552fc1b4aedae1a4f20ffbeab0a38cb05aee4
Amar Singh Yadav <amarprince9@gmail.com>
2023-02-25 13:51:30 +0530

    Setup Seach Video Result

--- src/components/SearchResults.jsx
+++ src/components/SearchResults.jsx
@@ -1,6 +1,27 @@
-import React from 'react';
+import React, { useContext, useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+import SearchResultVideoCard from './SearchResultVideoCard';
+import { Context } from '../context/contextApi';
+import { fetchDataFromAPI } from '../utils/api';
 
 const SearchResults = () => {
+  const [result, setResult] = useState();
+  const { searchQuery } = useParams();
+  const { setLoading } = useContext(Context);
+
+  useEffect(() => {
+    fetchSearchResult();
+  }, [searchQuery]);
+
+  const fetchSearchResult = () => {
+    setLoading(true);
+    fetchDataFromAPI(`search/?q=${searchQuery}`).then((res) => {
+      console.log(res?.contents);
+      setResult(res?.contents);
+      setLoading(false);
+    });
+  };
+
   return <div>SearchResults</div>;
 };
 
