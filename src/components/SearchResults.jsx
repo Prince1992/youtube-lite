b1daeac4954a44be10d78fcfd1faaca6fd78529e
Amar Singh Yadav <amarprince9@gmail.com>
2023-02-25 22:42:23 +0530

    Completed Video Deatails

--- src/components/SearchResults.jsx
+++ src/components/SearchResults.jsx
@@ -3,6 +3,7 @@ import { useParams } from 'react-router-dom';
 import SearchResultVideoCard from './SearchResultVideoCard';
 import { Context } from '../context/contextApi';
 import { fetchDataFromAPI } from '../utils/api';
+import LeftNav from './LeftNav';
 
 const SearchResults = () => {
   const [result, setResult] = useState();
@@ -22,7 +23,20 @@ const SearchResults = () => {
     });
   };
 
-  return <div>SearchResults</div>;
+  return (
+    <div className="flex flex-row h-[calc(100%-56px)">
+      <LeftNav />
+      <div className="grow w-[calc(100%-240px) h-full overflow-y-auto bg-black">
+        <div className="grid grid-cols-1 gap-2 p-5">
+          {result?.map((item, index) => {
+            if (item.type !== 'video') return false;
+            let video = item?.video;
+            return <SearchResultVideoCard key={video?.videoId} video={video} />;
+          })}
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default SearchResults;
